4f6812545047959ff46f73d9446bd8873d34b8bd
AmineCoder <116265410+W3Amine@users.noreply.github.com>
2023-05-02 16:36:11 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,7 @@ Route::apiResource('/products', ProductController::class);
 
 
 
-//this route is used to bring all the products that belong to one or more categories
-//when the user filter with categories he will get the products of each category he select
+// products filter by categories
 Route::get('/productsOfCategories', [ProductController::class, 'GetAllProductsOfCategories']);
 
 
