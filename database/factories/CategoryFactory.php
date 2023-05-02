1b3b054848bab12ea6917940d7d3987c3c570074
AmineCoder <116265410+W3Amine@users.noreply.github.com>
2023-05-02 16:33:17 +0100

    Update CategoryFactory.php

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,9 +5,6 @@ namespace Database\Factories;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
- */
 class CategoryFactory extends Factory
 {
 
