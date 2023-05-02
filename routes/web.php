3b8ad8cfdd59867cb93efb3eb68220a205cc2423
AmineCoder <116265410+W3Amine@users.noreply.github.com>
2023-05-02 16:12:19 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('app');
 });
+
+
 
 // Redirect any path to the app view # To avoid 404 error
 // Let vue handle routing # SPA
