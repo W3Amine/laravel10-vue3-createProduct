bc3ee70c1850ee567124aba048a7b464ef6f857e
AmineCoder <116265410+W3Amine@users.noreply.github.com>
2023-05-02 16:55:28 +0100

    Update PageNotFound.vue

--- resources/js/vue/pages/PageNotFound.vue
+++ resources/js/vue/pages/PageNotFound.vue
@@ -1,14 +1,9 @@
-
 <script setup>
-import NavBar from '../components/NavBar.vue';
-import TheFooter from '../components/TheFooter.vue';
 import TheHeader from '../components/TheHeader.vue';
 </script>
 
 <template>
-<NavBar />
 <TheHeader TheTitle="404 page not found :("/>
-
 <div class="row mx-auto col-md-8">
                 <div class="col-md-6 mx-auto">
                     <img src="https://cdn.dribbble.com/users/1408464/screenshots/6377404/404_illustration.png" alt="404"
@@ -22,9 +17,5 @@ import TheHeader from '../components/TheHeader.vue';
                     <router-link to="/" class="btn btn-primary">Go Home</router-link>
 
                 </div>
-
         </div>
-<TheFooter />
-
-
 </template>
