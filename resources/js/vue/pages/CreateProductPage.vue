ed5f63d3b33f7a8dd0ec72902256748f00e72816
AmineCoder <116265410+W3Amine@users.noreply.github.com>
2023-05-02 16:44:38 +0100

    Update CreateProductPage.vue

--- resources/js/vue/pages/CreateProductPage.vue
+++ resources/js/vue/pages/CreateProductPage.vue
@@ -1,23 +1,13 @@
-
 <script setup>
-
 import NavBar from '../components/NavBar.vue';
 import TheHeader from '../components/TheHeader.vue';
 import TheFooter from '../components/TheFooter.vue';
 import CreateProductForm from '../components/CreateProductForm.vue';
-
 </script>
 
-
-
 <template>
-
 <NavBar />
-
 <TheHeader TheTitle="Create a new Product :D"/>
-
 <CreateProductForm />
-
 <TheFooter />
-
 </template>
