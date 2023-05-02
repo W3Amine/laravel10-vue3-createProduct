4be90d964ffd631472009f8a5ef92995a6e71226
lolita <bkhit.amine.solicode@gmail.com>
2023-05-02 23:39:18 +0100

    .
    .

--- resources/js/vue/pages/CreateProductPage.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import TheHeader from '../components/TheHeader.vue';
-import CreateProductForm from '../components/CreateProductForm.vue';
-</script>
-
-<template>
-<TheHeader TheTitle="Create a new Product :D"/>
-<CreateProductForm />
-</template>
--- resources/js/vue/pages/ProductsPage.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import TheHeader from '../components/TheHeader.vue';
-import ProductsList from '../components/ProductsList.vue';
-</script>
-
-<template>
-  <TheHeader TheTitle="Explore our products" />
-  <ProductsList  />
-</template>
