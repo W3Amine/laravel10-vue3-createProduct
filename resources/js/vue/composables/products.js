ad4b3feb4bd8debd147c5d3aa5656def259a8554
AmineCoder <116265410+W3Amine@users.noreply.github.com>
2023-05-02 16:43:48 +0100

    Update products.js

--- resources/js/vue/composables/products.js
+++ resources/js/vue/composables/products.js
@@ -7,7 +7,6 @@ export function getProducts() {
     const router = useRouter();
     const ServerErrors = ref("");
 
-
     const getProductsData = async () => {
         let response = await axios.get("/api/products/");
         products.value = response.data.data;
@@ -16,24 +15,20 @@ export function getProducts() {
     const storeProduct = async (data , file ) => {
         ServerErrors.value = '';
         const formData = new FormData();
-
         formData.append('name', data.name);
         formData.append('price', data.price);
         formData.append('description', data.description);
         formData.append('categories[]', data.categories);
         formData.append('image', file.value);
 
-
         try {
         await axios.post("/api/products/" , formData);
         await router.push('/');
     } catch (e){
         if (e.response.status === 422) {
-
             for(const key in e.response.data.errors) {
                 ServerErrors.value += e.response.data.errors[key][0] + "<br>";
             }
-
         }
     }
         };
@@ -42,21 +37,16 @@ export function getProducts() {
         await axios.delete("/api/products/" + id);
     };
 
-
-
     const GetAllProductsOfCategories = async (ids) => {
         let string = "";
-       
         console.log([...ids]);
          string = "?";
         for (let  i = 0 ; i < [...ids].length ; i++) {
             string += 'ids[]=' + [...ids][i] + '&';
         }
-
         let response = await axios.get("/api/productsOfCategories" + string);
         products.value = response.data.data;
 
     };
-
     return { products, getProductsData, ServerErrors , storeProduct , destroyProduct , GetAllProductsOfCategories };
 }
