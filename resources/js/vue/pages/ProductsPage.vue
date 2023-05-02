def11801bcaf9449a97027a12e8366412067d78e
AmineCoder <116265410+W3Amine@users.noreply.github.com>
2023-05-02 16:56:15 +0100

    Update ProductsPage.vue

--- resources/js/vue/pages/ProductsPage.vue
+++ resources/js/vue/pages/ProductsPage.vue
@@ -1,33 +1,9 @@
-
 <script setup>
-import TheFooter from '../components/TheFooter.vue';
-import NavBar from '../components/NavBar.vue';
 import TheHeader from '../components/TheHeader.vue';
-import TheSideBar from '../components/TheSideBar.vue';
 import ProductsList from '../components/ProductsList.vue';
-
-
-
-
-
-
 </script>
 
-
-
 <template>
-    <NavBar />
-
-    <TheHeader TheTitle="Explore our products" />
-
-
-
-                <ProductsList  />
-
-
-
-
-    <TheFooter />
-
-
+  <TheHeader TheTitle="Explore our products" />
+  <ProductsList  />
 </template>
