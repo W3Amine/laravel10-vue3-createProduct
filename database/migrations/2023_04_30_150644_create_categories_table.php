9e9b9c960b5dd72e9fd53f2bdbb4099df2c9ff35
AmineCoder <116265410+W3Amine@users.noreply.github.com>
2023-05-02 16:34:39 +0100

    Update 2023_04_30_150644_create_categories_table.php

--- database/migrations/2023_04_30_150644_create_categories_table.php
+++ database/migrations/2023_04_30_150644_create_categories_table.php
@@ -6,9 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
@@ -19,9 +17,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('categories');
