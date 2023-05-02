d69601a1cd3e2e815389472aa536a482ec005e3e
AmineCoder <116265410+W3Amine@users.noreply.github.com>
2023-05-02 16:54:06 +0100

    Update Index.vue

--- resources/js/vue/Index.vue
+++ resources/js/vue/Index.vue
@@ -1,5 +1,12 @@
+<script setup>
+import NavBar from './components/NavBar.vue';
+import TheFooter from './components/TheFooter.vue';
+</script>
 
-<!-- the index.vue contain the navbar and the current page component -->
+
+<!-- the index.vue contain the navbar and the footer the current page component -->
 <template>
+<NavBar />
     <router-view></router-view>
+<TheFooter />
 </template>
