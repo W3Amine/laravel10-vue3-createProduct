aaf725cb5f2ce2f581e152b71e00f71308926880
AmineCoder <116265410+W3Amine@users.noreply.github.com>
2023-05-02 16:43:00 +0100

    Update categories.js

--- resources/js/vue/composables/categories.js
+++ resources/js/vue/composables/categories.js
@@ -3,15 +3,10 @@ import axios from "axios";
 
 export default function getCategories() {
     const categories = ref([]);
-
-
     const getCategoriesData = async () => {
         let response = await axios.get("/api/categories/");
         categories.value = response.data;
     };
-
-
-
 
     return { categories, getCategoriesData };
 }
